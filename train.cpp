fddde709e1b99244bd8baafbc224ff6d77b638ce
Ichinose Shogo <shogo82148@gmail.com>
2012-11-16 13:48:39 +0900

    reduce the number of file access

--- train.cpp
+++ train.cpp
@@ -14,7 +14,6 @@ int main(int argc, char** argv) {
 
     //initialize
     std::vector<double> D;
-    std::vector<signed char> labels;
     MODEL model, errors;
     {
         std::ifstream instances(instances_file);
@@ -25,7 +24,6 @@ int main(int argc, char** argv) {
             int label;
             is >> label;
             D.push_back(1.0);
-            labels.push_back(label);
             while(is >> h) {
                 model[h] = 0.0;
             }
@@ -33,21 +31,19 @@ int main(int argc, char** argv) {
     }
     model[""] = 0.0;
     errors[""] = 0.0;
-    const int num_instances = labels.size();
+    const int num_instances = D.size();
+    std::string h_best = "";
+    double e_best = 0.5;
+    double a = 0;
+    double a_exp = 1;
 
     for(int t = 0; t < 100; ++t) {
-        // calculate errors
+        // update & calculate errors
         double D_sum = 0;
         double D_sum_plus = 0;
-        for(int i = 0; i < num_instances; ++i) {
-            D_sum += D[i];
-            if(labels[i] > 0) {
-                D_sum_plus += D[i];
-            }
-        }
-        D_sum_plus /= D_sum;
+
         for(MODEL::iterator it = model.begin(); it!=model.end(); ++it) {
-            errors[it->first] = D_sum_plus;
+            errors[it->first] = 0;
         }
         {
             int i = 0;
@@ -56,23 +52,35 @@ int main(int argc, char** argv) {
             while(instances && getline(instances, line)) {
                 std::istringstream is(line);
                 std::string h;
+                std::vector<std::string> hs;
                 int label;
+                int prediction = -1;
                 is >> label;
-                D[i] /= D_sum;
-                const double d = D[i] * label;
                 while(is >> h) {
-                    errors[h] -= d;
+                    hs.push_back(h);
+                    if(h == h_best) prediction = +1;
+                }
+                if(label * prediction < 0) {
+                    D[i] *= a_exp;
+                } else {
+                    D[i] /= a_exp;
+                }
+                D_sum += D[i];
+                if(label > 0) D_sum_plus += D[i];
+                const double d = D[i] * label;
+                for(int j = 0; j < hs.size(); ++j) {
+                    errors[hs[j]] -= d;
                 }
                 i++;
             }
         }
 
         // select best classifier
-        std::string h_best = "";
-        double e_best = D_sum_plus;
+        e_best = D_sum_plus / D_sum;
+        h_best = "";
         for(MODEL::iterator it = errors.begin(); it!=errors.end(); ++it) {
             const std::string &h = it->first;
-            const double e = it->second;
+            const double e = (it->second + D_sum_plus) / D_sum;
             if(std::abs(0.5-e) > std::abs(0.5-e_best)) {
                 h_best = h;
                 e_best = e;
@@ -84,32 +92,13 @@ int main(int argc, char** argv) {
         if(e_best>1-1e-10) e_best = 1-1e-10;
 
         // update model
-        {
-            const double a = 0.5 * std::log((1-e_best)/e_best);
-            const double a_exp = std::exp(a);
-            int i = 0;
-            std::ifstream instances(instances_file);
-            std::string line;
-            while(instances && getline(instances, line)) {
-                std::istringstream is(line);
-                std::string h;
-                int label;
-                int prediction = -1;
-                is >> label;
-                while(is >> h) {
-                    if(h == h_best) {
-                        prediction = +1;
-                        break;
-                    }
-                }
-                if(label * prediction < 0) {
-                    D[i] *= a_exp;
-                } else {
-                    D[i] /= a_exp;
-                }
-                i++;
-            }
-            model[h_best] += a;
+        a = 0.5 * std::log((1-e_best)/e_best);
+        a_exp = std::exp(a);
+        model[h_best] += a;
+
+        // normalize
+        for(int i = 0; i < D.size(); ++i) {
+            D[i] /= D_sum;
         }
     }
 
