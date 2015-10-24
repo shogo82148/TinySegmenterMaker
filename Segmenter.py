d0f103b8f94dbc797a75f448d08bad26c242d470
Ichinose Shogo <shogo82148@gmail.com>
2015-10-24 21:00:51 +0900

    support python3

--- Segmenter.py
+++ Segmenter.py
@@ -20,56 +20,56 @@ class Segmenter(object):
         for pattern, sType in self.patterns:
             if pattern.match(ch):
                 return sType
-        return 'O'
+        return u'O'
 
     def addSentence(self, sentence):
         if not sentence:
             return
         learner = self.learner
-        tags = ['U', 'U', 'U']
-        chars = ['B3', 'B2', 'B1']
-        types = ['O', 'O', 'O']
+        tags = [u'U', u'U', u'U']
+        chars = [u'B3', u'B2', u'B1']
+        types = [u'O', u'O', u'O']
 
-        for word in sentence.split(' '):
-            tags.extend(['B'] + ['O'] * (len(word)-1))
+        for word in sentence.split(u' '):
+            tags.extend([u'B'] + [u'O'] * (len(word)-1))
             for ch in word:
-                chars.append(ch.encode('utf-8'))
+                chars.append(ch)
                 types.append(self.getType(ch))
         if len(tags) < 4:
             return
-        tags[3] = 'U'
+        tags[3] = u'U'
 
-        chars.extend(['E1', 'E2', 'E3'])
-        types.extend(['O', 'O', 'O'])
+        chars.extend([u'E1', u'E2', u'E3'])
+        types.extend([u'O', u'O', u'O'])
 
-        for i in xrange(4, len(chars) - 3):
-            label = +1 if tags[i] == 'B' else -1
+        for i in range(4, len(chars) - 3):
+            label = +1 if tags[i] == u'B' else -1
             learner.add_instance(self.getAttributes(i, tags, chars, types), label)
 
     def parse(self, sentence):
         if not sentence:
             return []
         learner = self.learner
-        tags = ['U', 'U', 'U', 'U']
-        chars = ['B3', 'B2', 'B1']
-        types = ['O', 'O', 'O']
+        tags = [u'U', u'U', u'U', u'U']
+        chars = [u'B3', u'B2', u'B1']
+        types = [u'O', u'O', u'O']
 
         for ch in sentence:
-            chars.append(ch.encode('utf-8'))
+            chars.append(ch)
             types.append(self.getType(ch))
-        chars.extend(['E1', 'E2', 'E3'])
-        types.extend(['O', 'O', 'O'])
+        chars.extend([u'E1', u'E2', u'E3'])
+        types.extend([u'O', u'O', u'O'])
 
         result = []
         word = chars[3]
-        for i in xrange(4, len(chars) - 3):
+        for i in range(4, len(chars) - 3):
             label = learner.predict(self.getAttributes(i, tags, chars, types))
             if label >= 0:
                 result.append(word)
-                word = ''
-                tags.append('B')
+                word = u''
+                tags.append(u'B')
             else:
-                tags.append('O')
+                tags.append(u'O')
             word += chars[i]
         result.append(word)
         return result
@@ -91,47 +91,47 @@ class Segmenter(object):
         p2 = tags[i-2]
         p3 = tags[i-1]
         attributes = set([
-                'UP1:' + p1,
-                'UP2:' + p2,
-                'UP3:' + p3,
-                'BP1:' + p1 + p2,
-                'BP2:' + p2 + p3,
-                'UW1:' + w1,
-                'UW2:' + w2,
-                'UW3:' + w3,
-                'UW4:' + w4,
-                'UW5:' + w5,
-                'UW6:' + w6,
-                'BW1:' + w2 + w3,
-                'BW2:' + w3 + w4,
-                'BW3:' + w4 + w5,
-                'TW1:' + w1 + w2 + w3,
-                'TW2:' + w2 + w3 + w4,
-                'TW3:' + w3 + w4 + w5,
-                'TW4:' + w4 + w5 + w6,
-                'UC1:' + c1,
-                'UC2:' + c2,
-                'UC3:' + c3,
-                'UC4:' + c4,
-                'UC5:' + c5,
-                'UC6:' + c6,
-                'BC1:' + c2 + c3,
-                'BC2:' + c3 + c4,
-                'BC3:' + c4 + c5,
-                'TC1:' + c1 + c2 + c3,
-                'TC2:' + c2 + c3 + c4,
-                'TC3:' + c3 + c4 + c5,
-                'TC4:' + c4 + c5 + c6,
-                'UQ1:' + p1 + c1,
-                'UQ2:' + p2 + c2,
-                'UQ3:' + p3 + c3,
-                'BQ1:' + p2 + c2 + c3,
-                'BQ2:' + p2 + c3 + c4,
-                'BQ3:' + p3 + c2 + c3,
-                'BQ4:' + p3 + c3 + c4,
-                'TQ1:' + p2 + c1 + c2 + c3,
-                'TQ2:' + p2 + c2 + c3 + c4,
-                'TQ3:' + p3 + c1 + c2 + c3,
-                'TQ4:' + p3 + c2 + c3 + c4,
+                u'UP1:' + p1,
+                u'UP2:' + p2,
+                u'UP3:' + p3,
+                u'BP1:' + p1 + p2,
+                u'BP2:' + p2 + p3,
+                u'UW1:' + w1,
+                u'UW2:' + w2,
+                u'UW3:' + w3,
+                u'UW4:' + w4,
+                u'UW5:' + w5,
+                u'UW6:' + w6,
+                u'BW1:' + w2 + w3,
+                u'BW2:' + w3 + w4,
+                u'BW3:' + w4 + w5,
+                u'TW1:' + w1 + w2 + w3,
+                u'TW2:' + w2 + w3 + w4,
+                u'TW3:' + w3 + w4 + w5,
+                u'TW4:' + w4 + w5 + w6,
+                u'UC1:' + c1,
+                u'UC2:' + c2,
+                u'UC3:' + c3,
+                u'UC4:' + c4,
+                u'UC5:' + c5,
+                u'UC6:' + c6,
+                u'BC1:' + c2 + c3,
+                u'BC2:' + c3 + c4,
+                u'BC3:' + c4 + c5,
+                u'TC1:' + c1 + c2 + c3,
+                u'TC2:' + c2 + c3 + c4,
+                u'TC3:' + c3 + c4 + c5,
+                u'TC4:' + c4 + c5 + c6,
+                u'UQ1:' + p1 + c1,
+                u'UQ2:' + p2 + c2,
+                u'UQ3:' + p3 + c3,
+                u'BQ1:' + p2 + c2 + c3,
+                u'BQ2:' + p2 + c3 + c4,
+                u'BQ3:' + p3 + c2 + c3,
+                u'BQ4:' + p3 + c3 + c4,
+                u'TQ1:' + p2 + c1 + c2 + c3,
+                u'TQ2:' + p2 + c2 + c3 + c4,
+                u'TQ3:' + p3 + c1 + c2 + c3,
+                u'TQ4:' + p3 + c2 + c3 + c4,
                 ])
         return attributes
