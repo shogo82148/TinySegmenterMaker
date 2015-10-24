c73ce168895de53183c1bc0d1dcccb7543c7f933
Ichinose Shogo <shogo82148@gmail.com>
2015-10-24 22:51:42 +0900

    run benchmark of python

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -5,10 +5,15 @@ set -xe
 # node
 cp ../tinysegmenter.js ./tinysegmenter.js
 node --version
-#node benchmark.js
+node benchmark.js
 
 # go
 mkdir -p tinysegmenter
 cp ../tinysegmenter.go tinysegmenter
 go version
 go run benchmark.go
+
+# python
+cp ../tinysegmenter.py ./tinysegmenter.py
+python --version
+python benchmark.py
