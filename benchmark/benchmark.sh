33a32812d11e9bc97f8c3fbe2f3060c07f88e796
Ichinose Shogo <shogo82148@gmail.com>
2015-10-24 22:28:52 +0900

    add benchmark of go

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -2,5 +2,13 @@
 
 set -xe
 
+# node
+cp ../tinysegmenter.js ./tinysegmenter.js
 node --version
-node benchmark.js
+#node benchmark.js
+
+# go
+mkdir -p tinysegmenter
+cp ../tinysegmenter.go tinysegmenter
+go version
+go run benchmark.go
