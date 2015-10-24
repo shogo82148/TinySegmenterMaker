b1dcbb1495bf8f5c4fff746cd2579f2fae553fdc
Ichinose Shogo <shogo82148@gmail.com>
2015-10-24 23:26:19 +0900

    fix syntacs error

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -14,4 +14,4 @@ start = time.time()
 for _ in range(100):
     segmenter.segment(text)
 elapsed_time = time.time() - start
-print ("elapsed_time:{0}".format(elapsed_time)) + "[sec]"
+print (("elapsed_time:{0}".format(elapsed_time)) + "[sec]")
