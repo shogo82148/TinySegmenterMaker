5a908a89d4f38e24d6e16d36a43f02e1e33d9aa4
Ichinose Shogo <shogo82148@gmail.com>
2015-10-29 00:53:31 +0900

    repeat 100 times

--- benchmark/benchmark.pl
+++ benchmark/benchmark.pl
@@ -4,9 +4,8 @@ use Encode;
 open my $fh, '<', 'timemachineu8j.txt';
 my $text = decode_utf8(do { local $/; <$fh> });
 
-warn "Perl version is too slow... so only repeat 10 times\n";
 my $start = time;
-for (1..10) {
+for (1..100) {
     tinysegmenter->segment($text);
 }
 my $end = time;
